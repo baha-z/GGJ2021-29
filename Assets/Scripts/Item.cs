fe07268ca33581989635f5ef39f66cafcaca04e2
baha-z <hector.vargas.infante@gmail.com>
2021-01-30 16:05:38 -0300

    custom text

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+    public ControlCursor controlCursor;
+    public Text displayText;
+    public string description;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,21 @@ public class Item : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnMouseOver()
+    {
+        controlCursor.CambiarCursor("mano");
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator = gameObject.GetComponent<Animator>();
+            animator.SetBool("Activo", true);
+            displayText.text = description;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        controlCursor.CambiarCursor("normal");
     }
 }
